f2b5786b2217b16acb2e8ac3bf547a1dd565cd5f
Admin <arunlakshman28@gmail.com>
2025-12-14 00:00:52 +0530

    updated files

---
+++ app.js
@@ -0,0 +1,12 @@
+let countPL = document.getElementById("countPL");
+
+console.log(countPL);
+
+let count = 0;
+
+
+function increment() {
+    count = count + 1;
+    countPL.innerText = count;
+    console.log(count);
+}
